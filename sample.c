70dee32f26b82ba22524864793f3700fedc18f62
AdastraJAP <adastrapja@gmail.com>
2026-04-05 21:40:46 +0800

    - A token array to store all tokens, not just print one-by-one   - A full lexing loop until TK_EOF   - File input instead of a hardcoded source string   - Proper fatal error handling   - Memory cleanup

---
+++ sample.c
@@ -0,0 +1,5 @@
+int main() {
+    int x = 42;
+    if (x >= 10) return x;
+    return 0;
+}
--- src/Tokenizer.c
+++ src/Tokenizer.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
 
-
-typedef enum{
+typedef enum {
     TK_INT,
     TK_IF,
     TK_ELSE,
@@ -12,7 +12,6 @@ typedef enum{
     TK_RETURN,
 
     TK_NUMBER,
-
     TK_IDENT,
 
     TK_PLUS,
@@ -20,15 +19,12 @@ typedef enum{
     TK_STAR,
     TK_SLASH,
 
-
     TK_EQ,
     TK_NEQ,
     TK_LT,
     TK_GT,
     TK_LEQ,
     TK_GEQ,
-
-    
     TK_ASSIGN,
 
     TK_LPAREN,
@@ -39,17 +35,28 @@ typedef enum{
     TK_COMMA,
 
     TK_EOF
+} Tokentype;
 
-}Tokentype;
+typedef struct {
+    Tokentype type;
+    const char *start;
+    int len;
+    int line;
+} Token;
 
+typedef struct {
+    Token *data;
+    int count;
+    int cap;
+} TokenList;
 
-typedef struct{
- Tokentype type;
- const char *start;
- int len;
- int line;
-
-}Token;
+typedef struct {
+    const char *src;
+    const char *cur;
+    const char *filename;
+    int line;
+    TokenList *out;
+} Lexer;
 
 const char *type_name(Tokentype k) {
     switch (k) {
@@ -64,7 +71,7 @@ const char *type_name(Tokentype k) {
         case TK_MINUS:  return "-";
         case TK_STAR:   return "*";
         case TK_SLASH:  return "/";
-        case TK_EQ:     return "=="; 
+        case TK_EQ:     return "==";
         case TK_NEQ:    return "!=";
         case TK_LT:     return "<";
         case TK_GT:     return ">";
@@ -82,166 +89,270 @@ const char *type_name(Tokentype k) {
     }
 }
 
-typedef struct {
-    const char *src;    
-    const char *cur;    
-    int         line;   
-} Lexer;
+static void fatal(const char *filename, int line, const char *fmt, ...) {
+    va_list args;
 
+    fprintf(stderr, "%s:%d: ", filename, line);
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    fprintf(stderr, "\n");
 
+    exit(1);
+}
 
- static char peek(Lexer *l) {
+static void tl_init(TokenList *tl) {
+    tl->cap = 64;
+    tl->count = 0;
+    tl->data = malloc((size_t)tl->cap * sizeof(Token));
+    if (!tl->data) {
+        fprintf(stderr, "fatal: out of memory\n");
+        exit(1);
+    }
+}
+
+static void tl_push(TokenList *tl, Token tok) {
+    if (tl->count == tl->cap) {
+        tl->cap *= 2;
+        tl->data = realloc(tl->data, (size_t)tl->cap * sizeof(Token));
+        if (!tl->data) {
+            fprintf(stderr, "fatal: out of memory\n");
+            exit(1);
+        }
+    }
+
+    tl->data[tl->count++] = tok;
+}
+
+static void tl_free(TokenList *tl) {
+    free(tl->data);
+    tl->data = NULL;
+    tl->count = 0;
+    tl->cap = 0;
+}
+
+static int at_end(Lexer *l) {
+    return *l->cur == '\0';
+}
+
+static char peek(Lexer *l) {
     return *l->cur;
 }
 
 static char advance(Lexer *l) {
     char c = *l->cur++;
-    if (c == '\n') l->line++;
+    if (c == '\n') {
+        l->line++;
+    }
     return c;
 }
 
 static int match(Lexer *l, char expected) {
-    if (*l->cur != expected) return 0;
+    if (at_end(l) || *l->cur != expected) {
+        return 0;
+    }
+
     l->cur++;
     return 1;
 }
 
-
-
-
+static void emit(Lexer *l, Tokentype type, const char *start, int len) {
+    Token t;
+    t.type = type;
+    t.start = start;
+    t.len = len;
+    t.line = l->line;
+    tl_push(l->out, t);
+}
 
 static Tokentype check_keyword(const char *start, int len) {
-    if (len == 3 && memcmp(start, "int",    3) == 0) return TK_INT;
-    if (len == 2 && memcmp(start, "if",     2) == 0) return TK_IF;
-    if (len == 4 && memcmp(start, "else",   4) == 0) return TK_ELSE;
-    if (len == 5 && memcmp(start, "while",  5) == 0) return TK_WHILE;
+    if (len == 3 && memcmp(start, "int", 3) == 0) return TK_INT;
+    if (len == 2 && memcmp(start, "if", 2) == 0) return TK_IF;
+    if (len == 4 && memcmp(start, "else", 4) == 0) return TK_ELSE;
+    if (len == 5 && memcmp(start, "while", 5) == 0) return TK_WHILE;
     if (len == 6 && memcmp(start, "return", 6) == 0) return TK_RETURN;
     return TK_IDENT;
 }
 
-
-static Token next_token(Lexer *l) {
-    
-    while (*l->cur && isspace((unsigned char)*l->cur))
+static void scan_one(Lexer *l) {
+    while (!at_end(l) && isspace((unsigned char)peek(l))) {
         advance(l);
-
-    Token t;
-    t.start = l->cur;
-    t.line  = l->line;
-    t.len   = 1;
-
-    
-    if (*l->cur == '\0') {
-        t.type = TK_EOF;
-        t.len  = 0;
-        return t;
     }
 
+    if (at_end(l)) {
+        emit(l, TK_EOF, l->cur, 0);
+        return;
+    }
+
+    const char *start = l->cur;
     char c = advance(l);
 
-    
-    if (isdigit((unsigned char)c)) {
-        while (isdigit((unsigned char)*l->cur))
-            advance(l);
-        t.type = TK_NUMBER;
-        t.len  = (int)(l->cur - t.start);
-        return t;
-    }
-
-    
-    if (isalpha((unsigned char)c) || c == '_') {
-        while (isalnum((unsigned char)*l->cur) || *l->cur == '_')
-            advance(l);
-        t.len  = (int)(l->cur - t.start);
-        t.type = check_keyword(t.start, t.len);
-        return t;
-    }
-
-    
     if (c == '/' && peek(l) == '/') {
-        while (*l->cur && *l->cur != '\n')
+        while (!at_end(l) && peek(l) != '\n') {
             advance(l);
-        return next_token(l);
+        }
+        return;
     }
 
-    
+    if (isdigit((unsigned char)c)) {
+        while (!at_end(l) && isdigit((unsigned char)peek(l))) {
+            advance(l);
+        }
+        emit(l, TK_NUMBER, start, (int)(l->cur - start));
+        return;
+    }
+
+    if (isalpha((unsigned char)c) || c == '_') {
+        while (!at_end(l) &&
+               (isalnum((unsigned char)peek(l)) || peek(l) == '_')) {
+            advance(l);
+        }
+        emit(l, check_keyword(start, (int)(l->cur - start)),
+             start, (int)(l->cur - start));
+        return;
+    }
+
     switch (c) {
-        case '+': t.type = TK_PLUS;   return t;
-        case '-': t.type = TK_MINUS;  return t;
-        case '*': t.type = TK_STAR;   return t;
-        case '/': t.type = TK_SLASH;  return t;
-        case '(': t.type = TK_LPAREN; return t;
-        case ')': t.type = TK_RPAREN; return t;
-        case '{': t.type = TK_LBRACE; return t;
-        case '}': t.type = TK_RBRACE; return t;
-        case ';': t.type = TK_SEMI;   return t;
-        case ',': t.type = TK_COMMA;  return t;
+        case '+': emit(l, TK_PLUS, start, 1); return;
+        case '-': emit(l, TK_MINUS, start, 1); return;
+        case '*': emit(l, TK_STAR, start, 1); return;
+        case '/': emit(l, TK_SLASH, start, 1); return;
+        case '(': emit(l, TK_LPAREN, start, 1); return;
+        case ')': emit(l, TK_RPAREN, start, 1); return;
+        case '{': emit(l, TK_LBRACE, start, 1); return;
+        case '}': emit(l, TK_RBRACE, start, 1); return;
+        case ';': emit(l, TK_SEMI, start, 1); return;
+        case ',': emit(l, TK_COMMA, start, 1); return;
         case '<':
-            if (match(l, '=')) { t.type = TK_LEQ; t.len = 2; }
-            else                  t.type = TK_LT;
-            return t;
+            if (match(l, '=')) {
+                emit(l, TK_LEQ, start, 2);
+            } else {
+                emit(l, TK_LT, start, 1);
+            }
+            return;
         case '>':
-            if (match(l, '=')) { t.type = TK_GEQ; t.len = 2; }
-            else                  t.type = TK_GT;
-            return t;
+            if (match(l, '=')) {
+                emit(l, TK_GEQ, start, 2);
+            } else {
+                emit(l, TK_GT, start, 1);
+            }
+            return;
         case '=':
-            if (match(l, '=')) { t.type= TK_EQ;     t.len = 2; }
-            else                  t.type = TK_ASSIGN;
-            return t;
+            if (match(l, '=')) {
+                emit(l, TK_EQ, start, 2);
+            } else {
+                emit(l, TK_ASSIGN, start, 1);
+            }
+            return;
         case '!':
-            if (match(l, '=')) { t.type= TK_NEQ; t.len = 2; return t; }
+            if (match(l, '=')) {
+                emit(l, TK_NEQ, start, 2);
+                return;
+            }
             break;
     }
 
-    
-    fprintf(stderr, "line %d: unexpected character '%c'\n", t.line, c);
-    return next_token(l);
+    fatal(l->filename, l->line, "unexpected character '%c'", c);
 }
 
-
-
-
-
-
-
-
-
-
-int main(){
-     const char *source =
-        "int add(int a, int b) {\n"
-        "    return a + b;\n"
-        "}\n"
-        "\n"
-        "int main() {\n"
-        "    int x = add(3, 7);\n"
-        "    if (x > 5) {\n"
-        "        return x;\n"
-        "    }\n"
-        "    return 0;\n"
-        "}\n";
-
+static TokenList lex_source(const char *src, const char *filename) {
+    TokenList tl;
     Lexer l;
-    l.src  = source;
-    l.cur  = source;
+
+    tl_init(&tl);
+
+    l.src = src;
+    l.cur = src;
+    l.filename = filename;
     l.line = 1;
+    l.out = &tl;
+
+    do {
+        scan_one(&l);
+    } while (tl.data[tl.count - 1].type != TK_EOF);
+
+    return tl;
+}
+
+static void dump_tokens(const TokenList *tl) {
+    int i;
 
     printf("%-6s  %-12s  %s\n", "LINE", "TYPE", "TEXT");
     printf("------  ------------  --------------------\n");
 
-    Token t;
-    do {
-        t = next_token(&l);
+    for (i = 0; i < tl->count; i++) {
+        const Token *t = &tl->data[i];
         printf("%-6d  %-12s  '%.*s'\n",
-               t.line,
-               type_name(t.type),
-               t.len,
-               t.start);
-    } while (t.type != TK_EOF);
-
-    return 0;
+               t->line,
+               type_name(t->type),
+               t->len,
+               t->start);
+    }
 }
 
+static char *read_file(const char *path) {
+    FILE *fp;
+    long size;
+    char *buffer;
 
+    fp = fopen(path, "rb");
+    if (!fp) {
+        fprintf(stderr, "could not open file: %s\n", path);
+        exit(1);
+    }
 
+    if (fseek(fp, 0, SEEK_END) != 0) {
+        fclose(fp);
+        fprintf(stderr, "could not seek file: %s\n", path);
+        exit(1);
+    }
 
+    size = ftell(fp);
+    if (size < 0) {
+        fclose(fp);
+        fprintf(stderr, "could not get file size: %s\n", path);
+        exit(1);
+    }
+
+    if (fseek(fp, 0, SEEK_SET) != 0) {
+        fclose(fp);
+        fprintf(stderr, "could not rewind file: %s\n", path);
+        exit(1);
+    }
+
+    buffer = malloc((size_t)size + 1);
+    if (!buffer) {
+        fclose(fp);
+        fprintf(stderr, "fatal: out of memory\n");
+        exit(1);
+    }
+
+    if (fread(buffer, 1, (size_t)size, fp) != (size_t)size) {
+        free(buffer);
+        fclose(fp);
+        fprintf(stderr, "could not read file: %s\n", path);
+        exit(1);
+    }
+
+    buffer[size] = '\0';
+    fclose(fp);
+    return buffer;
+}
+
+int main(int argc, char **argv) {
+    char *source;
+    TokenList tokens;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <source-file>\n", argv[0]);
+        return 1;
+    }
+
+    source = read_file(argv[1]);
+    tokens = lex_source(source, argv[1]);
+    dump_tokens(&tokens);
+
+    tl_free(&tokens);
+    free(source);
+    return 0;
+}
